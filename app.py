cbd5cf51d625e9ade11825c81e0aaa192c7bdab8
kangjimoon <kangjimoon@korea.ac.kr>
2025-02-04 16:55:19 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import pycountry
 # GitHub에서 CSV 데이터 로드
 @st.cache_data
 def load_data():
-    url = "https://raw.githubusercontent.com/kangjimoon/gisepa/main/esports_raw.csv"
+    url = "https://github.com/kangjimoon/gisepa/blob/main/Esports_Raw.csv"
     return pd.read_csv(url)
 
 df = load_data()
